16f449dcb730b47439c269d438961ed63d529f44
qq994300880 <994300880@qq.com>
2019-09-29 17:12:59 +0800

    Initial commit

---
+++ 13_插件/vue-myPlugin.js
@@ -0,0 +1,24 @@
+// Vue插件库
+(function (window) {
+    //需要向外暴露的插件对象
+    const MyPlugin = {};
+    //插件对象必须有一个Install()方法
+    MyPlugin.install = function (Vue, options) {
+        // 1. 添加全局方法或属性
+        Vue.myGlobalMethod = function () {
+            //逻辑
+            console.log('Vue函数对象的myGlobalMethod()')
+        };
+
+        // 2. 添加全局资源 => 自定义指令
+        Vue.directive('my-directive', function (el, binding) {
+            el.textContent = 'my-directive----' + binding.value;
+        });
+
+        // 4. 添加实例方法
+        Vue.prototype.$myMethod = function () {
+            console.log('vm $myMethod()');
+        }
+    };
+    window.MyPlugin = MyPlugin
+})(window);
